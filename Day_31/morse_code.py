dd921fbc3c4c4a83061978cc0d02ec93793dabcd
Karat Sidhu <karatsingh@gmail.com>
2022-03-20 09:43:22 +0530

    Day 31 - morse code

---
+++ Day_31/morse_code.py
@@ -0,0 +1,22 @@
+# You will use what you've learnt to create a text-based (command line) program that takes any String input and
+# converts it into Morse Code.
+#
+# You've created plenty of text-based programs in Days 1 -10, so look back at some of those projects if you don't
+# remember what a text-base program looks like.
+#
+# Wikipedia Entry for Morse Code
+#
+# The design, functionality, code style is all up to you. You're wearing the big-girl/big-boy pants now. So you get
+# to decide.
+
+
+from key import key
+
+# ask for input()
+morse_input = input("What would you like to translate to morse?: ")
+
+# split the input into individual letters
+
+# use those letters to find corresponding code from key.py
+
+# get the output and print it.
