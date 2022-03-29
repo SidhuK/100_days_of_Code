a4d1924536f14889e05f4000c1cc90bdb7fa3b64
Karat Sidhu <karatsingh@gmail.com>
2022-03-29 15:50:11 +0530

    Day 40 - Tictactoe game with python - other stuff added.

---
+++ Day_40/tictactoe.py
@@ -0,0 +1,141 @@
+# Import random function
+# use for selecting who goes first
+import random
+
+
+# create a class for making the game
+class TicTacToe:
+    # initialize the class
+    def __init__(self):
+        self.board = []
+
+    # create the board
+    def create_board(self):
+        for i in range(3):
+            row = []
+            for j in range(3):
+                row.append('-')
+            self.board.append(row)
+
+    # get the first player from the two
+    def get_random_first_player(self):
+        return random.randint(0, 1)
+
+    def fix_spot(self, row, col, player):
+        self.board[row][col] = player
+
+    # win conditions:
+    def is_player_win(self, player):
+        win = None
+
+        n = len(self.board)
+
+        # if all 3 rows have the same, player wins
+        # checking rows
+        for i in range(n):
+            win = True
+            for j in range(n):
+                if self.board[i][j] != player:
+                    win = False
+                    break
+            if win:
+                return win
+
+        # if all 3 columns have the same, player wins
+        # checking columns
+        for i in range(n):
+            win = True
+            for j in range(n):
+                if self.board[j][i] != player:
+                    win = False
+                    break
+            if win:
+                return win
+
+        # if all 3 diagonals have the same then player wins
+        # checking diagonals
+        win = True
+        for i in range(n):
+            if self.board[i][i] != player:
+                win = False
+                break
+        if win:
+            return win
+
+        win = True
+        for i in range(n):
+            if self.board[i][n - 1 - i] != player:
+                win = False
+                break
+        if win:
+            return win
+        return False
+
+        for row in self.board:
+            for item in row:
+                if item == '-':
+                    return False
+        return True
+
+    def is_board_filled(self):
+        for row in self.board:
+            for item in row:
+                if item == '-':
+                    return False
+        return True
+
+    # second player turn
+    def swap_player_turn(self, player):
+        return 'X' if player == 'O' else 'O'
+
+
+    # print end of game.
+    def show_board(self):
+        for row in self.board:
+            for item in row:
+                print(item, end=" ")
+            print()
+
+
+    # print start of game
+    def start(self):
+        self.create_board()
+
+        player = 'X' if self.get_random_first_player() == 1 else 'O'
+        while True:
+            print(f"Player {player} turn")
+
+            self.show_board()
+
+            # taking user input
+            row, col = list(
+                map(int, input("Enter row and column numbers to fix spot \n  Enter it in the form of row -space- "
+                               "column. \n Values should not exceed int=3 ").split()))
+            print()
+
+            # fixing the spot
+            self.fix_spot(row - 1, col - 1, player)
+
+            # checking whether current player is won or not
+            if self.is_player_win(player):
+                print(f"Player {player} wins the game!")
+                break
+
+            # checking whether the game is draw or not
+            if self.is_board_filled():
+                print("Match Draw!")
+                break
+
+            # swapping the turn
+            player = self.swap_player_turn(player)
+
+        # showing the final view of board
+        print()
+        self.show_board()
+
+
+# starting the game
+tic_tac_toe = TicTacToe()
+tic_tac_toe.start()
+
+
